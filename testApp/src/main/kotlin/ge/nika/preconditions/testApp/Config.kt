629399cbf4159d935992c505cc800f373757180c
NikaJamburia <nikajamburia161@gmail.com>
2025-04-08 22:59:52 +0400

    * fixed server config

--- testApp/src/main/kotlin/ge/nika/preconditions/testApp/Config.kt
+++ testApp/src/main/kotlin/ge/nika/preconditions/testApp/Config.kt
@@ -1,12 +1,19 @@
 package ge.nika.preconditions.testApp
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import ge.nika.preconditions.core.api.config.corePreconditions
+import ge.nika.preconditions.core.api.config.CorePreconditions
 import ge.nika.preconditions.library.Preconditions
 import ge.nika.preconditions.library.config.configurePreconditions
 
 private val preconditionsConfig = configurePreconditions {
-    registerModule { CorePreconditions() }
+    registerModule {
+        CorePreconditions.withAliases(
+            isP = listOf("=="),
+            and = listOf("&&"),
+            or = listOf("||"),
+            greaterThen = listOf(">"),
+        )
+    }
     registerPrecondition("IS_LESS_THEN", IsLessTranslator())
 }
 val PRECONDITIONS = Preconditions(preconditionsConfig)
